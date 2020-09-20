996c4369aad4ce6b577cd5714b82c39c5e0cf378
FernandoPieressa <fapieressa@uc.cl>
2020-09-20 14:21:52 -0300

    Fix no name in update request error

--- app/api/typeahead/update.controller.js
+++ app/api/typeahead/update.controller.js
@@ -5,11 +5,15 @@
  * @param {string} req.body.name - name to update popularity
  */
 function update(req, res) {
-	const result = structure.update_word(req.body.name.toLowerCase());
-	if (!result) {
+	if (req.body.name) {
+		const result = structure.update_word(req.body.name.toLowerCase());
+		if (!result) {
+			res.sendStatus(400);
+		} else {
+			res.status(201).send(result);
+		}
+	} {
 		res.sendStatus(400);
-	} else {
-		res.status(201).send(result);
 	}
 }
 
